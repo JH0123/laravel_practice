4d6f45b48cedb43bcccb65a4527ae97ff3d80b12
Ju-hyeon <mycom12317@gmail.com>
2021-09-30 20:27:46 +0900

    index

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -13,7 +14,8 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
+        // $posts = Post::latest()->paginate(10);
+        return view('posts.index');
     }
 
     /**
