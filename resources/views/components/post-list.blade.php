ec2479507c2afb77e2016c1ea05a893ce54582e7
Ju-hyeon <mycom12317@gmail.com>
2021-10-05 19:13:15 +0900

    bootstrap

--- resources/views/components/post-list.blade.php
+++ resources/views/components/post-list.blade.php
@@ -1,5 +1,5 @@
 <div class="m-4 p-4">
-    <table class="table">
+  <table class="table">
   <thead>
     <tr>
       <th scope="col">제목</th>
@@ -7,17 +7,17 @@
       <th scope="col">작성일</th>
     </tr>
   </thead>
-        <tbody>
-            {{-- 게시글 표시 --}}
-            @foreach ($posts as $post)
-                <tr>
-                    <td>{{ $post->title }}</td>
-                    <td>{{ $post->writer->name }}</td>
-                    <td>{{ $post->created_at->diffForHumans() }}</td>
-                    {{-- diffForHumans()은 1개월까지의 날짜를 표시한다 --}}
-                </tr>
-            @endforeach
-        </tbody>
+  <tbody>
+    {{-- 게시글 표시 --}}
+    @foreach ($posts as $post)
+      <tr>
+        <td>{{ $post->title }}</td>
+        <td>{{ $post->writer->name }}</td>
+        <td>{{ $post->created_at->diffForHumans() }}</td>
+        {{-- diffForHumans()은 1개월까지의 날짜를 표시한다 --}}
+      </tr>
+   @endforeach
+    </tbody>
     </table>
     {{-- pagination --}}
     {{ $posts->links() }}
