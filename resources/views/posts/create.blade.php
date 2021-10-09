bde049de1d4c3b96e2dd8034c41b69eff27431e8
Ju-hyeon <mycom12317@gmail.com>
2021-10-09 19:41:25 +0900

    create edit

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -9,33 +9,35 @@
     </x-slot>
 
     <div class="m-4 p-4">
-    {{-- 파일 업로드 --}}
-    <form class="row g-3" action="{{ route('posts.store') }}"
-    method="post" enctype="multipart/form-data">
-    @csrf{{-- 없으면 비활성화로 인해 페이지가 만료되었다는 페이지를 표시한다 --}}
+      {{-- 파일 업로드 --}}
+      <form class="row g-3" action="{{ route('posts.store') }}"
+      method="post" enctype="multipart/form-data">
+      @csrf{{-- 없으면 비활성화로 인해 페이지가 만료되었다는 페이지를 표시한다 --}}
 
-    <div class="col-12 m-2">
-    <label for="title" class="form-label">Title</label>
-    {{-- 오래된 데이터 표시? --}}
-    <textarea class="form=control" name="title" id="title">{{ old('title') }}</textarea>
-    {{-- 입력을 하지 않을 시 에러메시지 출력 --}}  
-    @error('title')
-      <span class="text-red-800">{{ $message }}</span>
-    @enderror
-  </div>
+        <div class="col-12 m-2">
+          <label for="title" class="form-label">Title</label>
+          {{-- 오래된 데이터 표시? --}}
+          <textarea class="form-control" name="title" id="title">{{ old('title') }}</textarea>
+          
+          {{-- 입력을 하지 않을 시 에러메시지 출력 --}}  
+          @error('title')
+            <span class="text-red-800">{{ $message }}</span>
+          @enderror
+        </div>
 
-  <div class="col-12 m-2">
-    <label for="content" class="form-label">Content</label>
-    <textarea class="form=control" name="content" id="content">{{ old('content') }}</textarea>
-    @error('content')
-      <span class="text-red-800">{{ $message }}</span>
-    @enderror
-  </div>
+        <div class="col-12 m-2">
+          <label for="content" class="form-label">Content</label>
+          <textarea class="form-control" name="content" id="content">{{ old('content') }}</textarea>
 
-  <div class="col-12 m-2">
-  <button type="submit" class="btn btn-info hover:bg-blue-700 font-bold text-white">Submit</button>
-  </div>
+          @error('content')
+            <span class="text-red-800">{{ $message }}</span>
+          @enderror
+        </div>
+
+        <div class="col-12 m-2">
+          <button type="submit" class="btn btn-info hover:bg-blue-700 font-bold text-white">Submit</button>
+        </div>
   
-  </form>
-  </div>
+      </form>
+    </div>
 </x-app-layout>
