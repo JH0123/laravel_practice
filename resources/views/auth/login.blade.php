68e772a393cc20718b3ae89e629f63c602d2ce67
Ju-hyeon <mycom12317@gmail.com>
2021-10-16 00:44:31 +0900

    login / register dark mode

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -36,19 +36,19 @@
             <div class="block mt-4">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" name="remember">
-                    <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    <span class="ml-2 text-sm text-gray-600 dark:text-gray-300">{{ __('Remember me') }}</span>
                 </label>
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('register'))
-                    <a class="underline text-sm mr-4 text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
+                    <a class="underline text-sm mr-4 text-gray-600 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-500" href="{{ route('register') }}">
                         {{ __('Register') }}
                     </a>
                 @endif
 
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 dark:text-gray-300 dark:hover:text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
