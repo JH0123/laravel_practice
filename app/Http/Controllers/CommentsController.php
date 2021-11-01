8e363bcdf34140536b9b2978af0c3fe9c08ec9ec
Ju-hyeon <mycom12317@gmail.com>
2021-11-01 17:49:16 +0900

    comment

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -7,15 +7,15 @@ use Illuminate\Http\Request;
 
 class CommentsController extends Controller
 {
-    public function index($post_id)
+    public function index($postId)
     {
         // 댓글 리스트
         // latest()는 데이터베이스에서 가장 최근의 데이터 세트를 가져온다
         // 즉, 데이터를 시간순으로 정렬하기 위해 'created_at 열을 사용하여 가져온 데이터를 정렬
-        $comments = Comment::where('post_id', $post_id)->latest()->get();
+        $comments = Comment::where('post_id', $postId)->latest()->get();
         return $comments;
     }
-    public function store(Request $request, $post_id)
+    public function store(Request $request, $postId)
     {
         // 댓글 등록
         $request->validate(['comment' => 'required']);
@@ -23,7 +23,7 @@ class CommentsController extends Controller
         $comment = Comment::create([
             'comment' => $request->input('comment'),
             'user_id' => auth()->user()->id,
-            'post_id' => $post_id
+            'post_id' => $postId
         ]);
         return $comment;
     }
