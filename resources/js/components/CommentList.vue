a10940295e7c310b74d5f587bf2e67719e3bbce4
Ju-hyeon <mycom12317@gmail.com>
2021-11-05 15:25:02 +0900

    comment

--- resources/js/components/CommentList.vue
+++ resources/js/components/CommentList.vue
@@ -1,25 +1,77 @@
 <template>
   <div>
+    <div class="bg-light p-2">
+      <label>댓글 작성</label>
+      <div class="d-flex flex-row align-items-start">
+        <textarea
+          v-model="newComment"
+          class="form-control ml-1 shadow-none textarea"
+        ></textarea>
+      </div>
+      <div class="mt-2 text-right">
+        <button
+          @click="addComment"
+          class="btn btn-primary btn-sm shadow-none"
+          type="button"
+        >
+          등록
+        </button>
+      </div>
+    </div>
     <button @click="getComments" class="btn btn-default">댓글 불러오기</button>
     <comment-item
-      v-for="(comment, index) in comments"
+      v-for="(comment, index) in comments.data"
       :key="index"
       :comment="comment"
+    />
+    <pagination
+      @pageClicked="getPage($event)"
+      v-if="comments.links != null"
+      :links="comments.links"
     />
   </div>
 </template>
 
 <script>
 import CommentItem from "./CommentItem.vue";
+import Pagination from "./Pagination.vue";
 export default {
   props: ["post", "loginuser"],
-  components: { CommentItem },
+  components: { CommentItem, Pagination },
   data() {
     return {
       comments: [],
+      newComment: "",
     };
   },
   methods: {
+    addComment() {
+      if (this.newComment == "") {
+        alert("글을 입력해주세요!");
+        return;
+      }
+      axios
+        .post("/comments/" + this.post.id, { comment: this.newComment })
+        .then((response) => {
+          console.log(response.data);
+          this.getComments();
+          this.newComment = "";
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    getPage(url) {
+      console.log(url);
+      axios
+        .get(url)
+        .then((response) => {
+          this.comments = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
     getComments() {
       // this.comments = [
       //   "1st comment",
