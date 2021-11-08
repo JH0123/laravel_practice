36d5103ad2fc512d285fae418f8140b78c3e3978
Ju-hyeon <mycom12317@gmail.com>
2021-11-08 17:50:51 +0900

    comment

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,9 @@ Route::get('/search', [PostsController::class], "search");
 
 Route::post("/like/{post}", [LikesController::class, "store"])->middleware(["auth"])->name("like.store");
 
-Route::post("/comments/{postId}", [CommentsController::class], "store")->middleware(['auth']);
-Route::get('/comments/{postId}', [CommentsController::class, 'index'])->middleware(['auth']);
-Route::delete("/comments/{commentId}", [CommentsController::class], "destroy")->middleware(['auth']);
-Route::patch("/comments/{commentId}", [CommentsController::class], "update")->middleware(['auth']);
+Route::post("/comments/{postId}", [CommentsController::class, "store"])->middleware(['auth']);
+Route::get('/comments/{postId}', [CommentsController::class, "index"])->middleware(['auth']);
+Route::delete("/comments/{commentId}", [CommentsController::class, "destroy"])->middleware(['auth']);
+Route::patch("/comments/{commentId}", [CommentsController::class, "update"])->middleware(['auth']);
 
 require __DIR__ . '/auth.php';
